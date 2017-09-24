091a206730674bb8a8b6b4bf59ab7b9ffbc0363e
Siji <rachtom89@gmail.com>
2017-09-23 20:23:03 -0700

    Categories skeleton implementation.

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -81,14 +81,4 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     
     // MARK: - Search bar delegate
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
